11a7c868bdb8533254e3d5368b514f87a97951b4
Luis Carrazco <luiscarrazco@gmail.com>
2019-01-14 10:06:32 -0700

    Integers and Arrays

--- main.js
+++ main.js
@@ -32,6 +32,7 @@
 // }
 
 //Integers
+
 // var cars = (((((4 + 2)* 8) / 2) % 2)
 // cars
 // var cars = 12
@@ -42,3 +43,45 @@
 //4 == 6
 //4 == '4'
 
+//Arrays
+// var users = ["Matt", "Cam", "Barbara", "Bryant", "Joseph"]
+// console.log(users[2])
+
+// var users = ["Matt", 2, "Cam", {user: "Barbara"}, ["array", "array2"], "Joseph"]
+// console.log(users[3].user)
+
+//Array Methods
+
+// var fruits = ["apple", "banana", "grape"]
+// console.log(fruits)
+
+// //push() one of the most used array methods****
+// fruits.push("strawberry")
+// console.log(fruits)
+
+// //pop()
+// fruits.pop()
+// console.log(fruits)
+// fruits.splice(1,1)
+// console.log(fruits)
+// fruits.length
+
+//Array excercises
+
+// Declare a variable with 5 different music artists
+var artists = ["Madonna", "Elvis Presley", "Frank Sinatra", "Barry White", "Freddie Mercury"]
+
+// print the variable
+console.log(artists)
+
+// Print the 4th artist on the Array with a console.log
+console.log(artists[3])
+
+// Add another artist to Array for a total of 6 artist then print the array
+artists.push("Jose Jose")
+console.log(artists)
+// Remove the 4th artist on the list and then print the list
+artists.splice(3,1)
+console.log(artists)
+// Print the array in reverse
+console.log(artists.reverse())
