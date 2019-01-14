336d4504f05a033b1f2fa89a1f261a67265be4a9
Luis Carrazco <luiscarrazco@gmail.com>
2019-01-14 09:18:01 -0700

    Integers

--- main.js
+++ main.js
@@ -1,4 +1,44 @@
-var x = "john";
-var y = "doe";
-var c = " "
-x + c + y
+//Operators
+
+// var x = "john";
+// var y = "doe";
+// var c = " "
+// x + c + y
+
+// var x = "5";
+// var y = 5;
+// (5 == 5) ? "yes its equal" : "not equal"
+
+// var user = {
+//     name: 'joe',
+//     age: 29
+// }
+
+// if(user.name == 'joe' && user.age == 29) {
+//     console.log('HEY LET THE USER IN')
+// } else {
+//     console.log('Dont give access')
+// }
+
+// var user = {
+//     name: 'joe',
+//     age: 29
+// }
+
+// if(user.name == 'bryant') {
+//     console.log('HEY LET THE USER IN')
+// } else {
+//     console.log('Dont give access')
+// }
+
+//Integers
+// var cars = (((((4 + 2)* 8) / 2) % 2)
+// cars
+// var cars = 12
+// var bikes = 4
+// cars * bikes
+//4 < 6
+//4 > 6
+//4 == 6
+//4 == '4'
+
